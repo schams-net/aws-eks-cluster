6eddf4826adeb56cc85833bfb62ce6d8c3ec29ff
Michael Schams <noreply@example.com>
2023-05-17 13:14:36 +1000

    Restrict access to the RDS Aurora cluster

--- Terraform/modules/rds-aurora-cluster/security_group_rds.tf
+++ Terraform/modules/rds-aurora-cluster/security_group_rds.tf
@@ -12,15 +12,14 @@
 resource "aws_security_group" "rds" {
     #name = "${lower(var.tags.Name)}-rds-aurora"
     name = "${var.tags.Name}-AuroraCluster"
-    description = "${var.tags.Name} RDS Aurora cluster"
+    description = "[${var.tags.Name}] RDS Aurora cluster"
     vpc_id = var.vpc.id
     ingress {
         from_port = 3306
         to_port = 3306
         protocol = "tcp"
-        # @TODO
-        #cidr_blocks = ["10.10.110.0/24", "10.10.120.0/24", "10.10.130.0/24"]
-        cidr_blocks = ["0.0.0.0/0"]
+        #cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.subnets[*].cidr_block
     }
     tags = merge(var.tags, {
         Name = "[${var.tags.Name}] RDS Aurora cluster"
