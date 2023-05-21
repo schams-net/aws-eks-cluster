c491a060f72d46deb24c894f5e71b099d6095683
Michael Schams <noreply@example.com>
2023-05-21 17:04:13 +1000

    Add an IAM policy for the Amazon EFS CSI Driver

--- Terraform/modules/iam/role_eks_service_account.tf
+++ Terraform/modules/iam/role_eks_service_account.tf
@@ -5,3 +5,21 @@ resource "aws_iam_role" "irsa" {
     assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
     name = "${var.tags.Name}-IAMRoleForKubernetesServiceAccount"
 }
+
+# ------------------------------------------------------------------------------
+
+# Policy for the Amazon EFS CSI Driver
+resource "aws_iam_policy" "efs_csi" {
+    name = "${var.tags.Name}-EFS-CSI-Driver"
+    path = "/"
+    description = "Policy for the Amazon EFS CSI Driver"
+    policy = file("${path.module}/json/efs-csi-driver-policy.json")
+    tags = merge(var.tags, {
+        Name = "${var.tags.Name}-EFS-CSI-Driver"
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "efs_csi" {
+    policy_arn = aws_iam_policy.efs_csi.arn
+    role = aws_iam_role.irsa.name
+}
