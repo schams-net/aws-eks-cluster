9d76fbe9c94558e3ca07b4903c09e0828d4c3697
Michael Schams <noreply@example.com>
2023-06-19 11:52:06 +1000

    Install AWS load balancer controller through an Helm chart

--- Terraform/modules/helm/helm.tf
+++ Terraform/modules/helm/helm.tf
@@ -1,1 +1,30 @@
 # ...
+
+# AWS load balancer controller
+# Elastic Load Balancing
+# https://aws.amazon.com/elasticloadbalancing/
+resource "helm_release" "aws-load-balancer-controller" {
+    name = "aws-load-balancer-controller"
+    repository = "https://aws.github.io/eks-charts"
+    chart = "aws-load-balancer-controller"
+    namespace = var.namespace
+
+    set {
+        name = "clusterName"
+        value = var.eks_cluster.name
+    }
+
+    set {
+        name = "serviceAccount.create"
+        value = "true"
+    }
+
+    set {
+        name = "serviceAccount.name"
+        value = "aws-load-balancer-controller"
+    }
+
+    depends_on = [
+        var.eks_cluster
+    ]
+}
