c8ef1d75c2f7ff3302f5fce32ae57c905d30852e
Michael Schams <noreply@example.com>
2023-06-21 10:11:57 +1000

    Attach IAM policy to IAM node group role
    This change lets the entire Kubernetes node group access any secrets
    stored in the Secrets Manager. IAM roles for service accounts are not
    required anymore.

--- Terraform/modules/iam/role_eks_service_account.tf
+++ Terraform/modules/iam/role_eks_service_account.tf
@@ -1,7 +1,7 @@
 # IAM Roles for Service Accounts (IRSA)
 
 # IAM role "*-IAMRoleForKubernetesServiceAccount"
-resource "aws_iam_role" "irsa" {
-    assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-    name = "${var.tags.Name}-IAMRoleForKubernetesServiceAccount"
-}
+#resource "aws_iam_role" "irsa" {
+#    assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+#    name = "${var.tags.Name}-IAMRoleForKubernetesServiceAccount"
+#}
